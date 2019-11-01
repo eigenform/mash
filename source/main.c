e8ace4e7be7a968e1ecb589ef04bdd485d092297
meta <hosaka-corp@users.noreply.github.com>
2019-11-01 06:13:24 -0500

    subframe polling definitely works

--- source/main.c
+++ source/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <gccore.h>
 #include <ogcsys.h>
 #include <ogc/pad.h>
@@ -19,16 +20,16 @@ static u32 total_clicks;
 
 PADStatus status[4];
 
-static u8 sample[0x1000];
-static u8 obs[0x1000];
+static u8 sample[0x100];
+static u8 obs[0x100];
+static u8 last;
 static u32 sidx = 0;
-static u8 current_state = 0;
 
 void si_cb()
 {
 	if (cb_cnt == 0)
 	{	
-		memset(sample, 0, sizeof(sample));
+		last = sample[sidx-1];
 		memset(obs, 0, sizeof(obs));
 		sidx = 0;
 	}
@@ -36,15 +37,20 @@ void si_cb()
 	PAD_Read(status);
 	if (status[0].button & PAD_BUTTON_A)
 	{
-		sample[sidx] = 1;
 		if (sidx > 0)
 		{
 			if (sample[sidx-1] == 0)
 			{
-				total_clicks++;
 				obs[sidx] = 1;
+				total_clicks++;
 			}
 		}
+		else if ((sidx == 0) && (last == 0))
+		{
+				obs[sidx] = 1;
+				total_clicks++;
+		}
+		sample[sidx] = 1;
 	}
 	else
 	{
@@ -53,12 +59,6 @@ void si_cb()
 
 	sidx++;
 	cb_cnt++;
-
-	//PAD_ScanPads();
-	//u32 pressed = PAD_ButtonsDown(0);
-	//if (pressed & PAD_BUTTON_A)
-	//	total_clicks++;
-	//SI_RegisterPollingHandler(si_cb);
 }
 
 
@@ -70,7 +70,6 @@ int main(int argc, char **argv)
 
 	// Set the underlying polling rate to 1ms
 	SI_SetSamplingRate(1);
-	SI_RegisterPollingHandler(si_cb);
 
 	rmode = VIDEO_GetPreferredMode(NULL);
 
@@ -91,70 +90,47 @@ int main(int argc, char **argv)
 	sleep(1);
 
 	u32 frame = 0;
-	u64 last_frame_time;
-	u64 current_frame_time;
-	u16 top_vct, top_hct;
-	u16 mid_vct, mid_hct;
-	u16 bot_vct, bot_hct;
 	cb_cnt = 0;
+	SI_RegisterPollingHandler(si_cb);
+	u32 hi_perframe = 0;
+	u32 perframe = 0;
 	while(1) 
 	{
-		top_vct = *vct;
-		top_hct = *hct;
-
-		//PAD_ScanPads();
-		//u32 pressed = PAD_ButtonsDown(0);
-		//if (pressed & PAD_BUTTON_A)
-		//	total_clicks++;
-		//// We return to the launcher application via exit
-		//if ( pressed & PAD_BUTTON_START )
-		//	exit(0);
-
 		// Wait for the next frame
-		mid_vct = *vct;
-		mid_hct = *hct;
 
 		VIDEO_WaitVSync();
-		bot_vct = *vct;
-		bot_hct = *hct;
 
 		// This is the start of a frame
 		frame++;
 
 		printf("\x1b[10;0H");
-		printf("frame %08d", frame);
+		printf("Current frame: %08d", frame);
 
 		printf("\x1b[11;0H");
-		printf("total clicks %08d", total_clicks);
-
-		printf("\x1b[12;0H");
-		current_frame_time = gettime();
-		printf("time  %016llx", current_frame_time);
+		printf("Total clicks:  %08d", total_clicks);
 
 		printf("\x1b[13;0H");
-		printf("since %016llx", current_frame_time - last_frame_time);
-		last_frame_time = current_frame_time;
-
+		printf("Samples/frame: %d (samples=%d)", cb_cnt, sidx);
 		printf("\x1b[14;0H");
-		printf("top of loop %04x %04x", top_vct, top_hct);
-		printf("\x1b[15;0H");
-		printf("before wait %04x %04x", mid_vct, mid_hct);
-		printf("\x1b[16;0H");
-		printf("bottom loop %04x %04x", bot_vct, bot_hct);
-		printf("\x1b[17;0H");
-		printf("cb_cnt %d, sidx %d", cb_cnt, sidx);
-		printf("\x1b[18;0H");
+		printf("Sample map:    ");
 		for (int i = 0; i < sidx; i++)
 			printf("%d ", sample[i]);
 		printf("\n");
 
-		printf("observed on sample ");
+		printf("\x1b[15;0H");
+		for (int j = 0; j < sidx; j++)
+			if (obs[j] == 1) perframe++;
+		if (perframe > hi_perframe)
+			hi_perframe = perframe;
+		printf("Max clicks/frame: %d\n", hi_perframe); 
+		perframe = 0;
+
+		printf("Latest active sample: ");
 		for (int j = 0; j < sidx; j++)
 		{
 			if (obs[j] == 1)
-				printf("%d, ", sidx);
+				printf("%d, ", j);
 		}
-
 		cb_cnt = 0;
 
 	}
